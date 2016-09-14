fb28924579332296cb54b39823526b33844a98ad
Adrien Rahier <adrien.rahier@gmail.com>
2016-09-14 20:48:47 +0100

    Some code reorganization / refacto

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # twitterwords
-A phantomJs / casperJs which scan the twitter profile of the twitter users
+A phantomJs / casperJs project which scan the twitter profile of a given list of words
+
+
+## IDEA : 
+
+Use it to scan the names per country
+
+## Strategy :
+
+A first script (dev/main1.js) is getting all the existing occurences. 
+A second script (dev/main2.js) can help really checking if the word exist or not (but there is a limit...)
+
+
+##TODO :
+
+- create a script which is querying google looking for "@auser" occurences
+- find the list of names
--- dev/main1.js
+++ dev/main1.js
@@ -1,5 +1,4 @@
-//https://twitter.com/users/username_available?username=conte
-// Launch command : casperjs test3.js
+// Launch command : casperjs main1.js
 
 /*
 
@@ -9,11 +8,10 @@
 
 	- Il se peut que le nom d’utilisateur soit utilisé par un compte suspendu ou désactivé. Les noms d’utilisateur de ces comptes ne peuvent pas être utilisés immédiatement. Vous devrez donc utiliser un autre nom d’utilisateur.
 
-
-
 */
 
-/* var casper = require('casper').create();
+
+var casper = require('casper').create();
 var startTime = Date.now();
 var fs = require('fs');
 
@@ -21,7 +19,7 @@ var finalDatas = [];
 
 casper.start('about:blank');
 
-var usernames = ["crayon", "stylo"];
+var usernames = ["crayon", "domino"];
 
 casper.each(usernames, function(casper, username) {
   casper.thenOpen("http://twitter.com/" + username, function() {
@@ -29,7 +27,7 @@ casper.each(usernames, function(casper, username) {
   		result.username = username;
   		result.exist = this.getTitle();
   		finalDatas.push(result);
-  		this.echo("Done: " + username);
+      this.echo(username, "COMMENT");
     });
 });
 
@@ -38,36 +36,3 @@ casper.run(function() {
 	this.echo("Done in " + (Date.now() - startTime)/1000 + "s", "INFO");
 	this.exit();
 });
-
-*/
-
-// $ casperjs test3.js --web-security=no
-
-var casper = require('casper').create();
-var wsurl = 'https://twitter.com/users/username_available?username=conte';
-
-casper.start();
-
-var utils = require('utils');
-
-casper.options.onResourceRequested = function(C, requestData, request) {
-    //utils.dump(requestData.headers);
-};
-casper.options.onResourceReceived = function(C, response) {
-    utils.dump(response);
-};
-
-casper.open(wsurl, {
-  method: 'get',
-   headers: {
-       'Content-Type': 'application/json; charset=utf-8'
-   },
-   encoding: 'utf8'
-}).then(function(data) {
-    //this.echo('GOT it.' + data);
-    //require('utils').dump(data);
-});
-
-casper.run();
-
-
